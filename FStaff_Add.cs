e696fed797b84284b154bc74723c9a617a1c6e6e
Minh <minhvuongakira@gmail.com>
2024-04-16 09:56:42 +0700

    create FStaff for add and update

---
+++ FStaff_Add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MilkTeaShop
+{
+    public partial class FStaff_Add : Form
+    {
+        public FStaff_Add()
+        {
+            InitializeComponent();
+        }
+    }
+}

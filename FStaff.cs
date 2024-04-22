b89efdbdcf9e6f6a03baa9438d18698820275140
Minh <minhvuongakira@gmail.com>
2024-04-22 11:12:40 +0700

    thêm các view trạng thái làm việc

--- FStaff.cs
+++ FStaff.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,7 @@ namespace MilkTeaShop
         {
             InitializeComponent();
             LoadStaff();
+            cbbWorkStatus.Text = "Đang làm việc";
         }
         public void LoadStaff()
         {
@@ -62,12 +64,92 @@ namespace MilkTeaShop
             }
         }
 
+        public void LoadStaff_Working()
+        {
+            flpanelListStaff.Controls.Clear();
+            string sqlQuery = "SELECT * FROM V_ThongTinNhanVienDangLamViec";
+            using (SqlConnection conn = new SqlConnection(conStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sqlQuery, conn))
+                {
+                    using (SqlDataReader dataReader = cmd.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            UC_Staff staff = new UC_Staff();
+                            staff.LblID.Text = dataReader["MaNV"].ToString();
+                            staff.LblName.Text = dataReader["HoTen"].ToString();
+                            staff.LblSex.Text = dataReader["GioiTinh"].ToString();
+                            DateTime dob = dataReader.GetDateTime(dataReader.GetOrdinal("NgaySinh"));
+                            staff.LblDob.Text = dob.ToString("yyyy-MM-dd");
+                            staff.LblAddress.Text = dataReader["DiaChi"].ToString();
+                            staff.LblPhone.Text = dataReader["SDT"].ToString();
+                            DateTime ngayTuyenDung = dataReader.GetDateTime(dataReader.GetOrdinal("NgayTuyenDung"));
+                            staff.LblNgayTuyenDung.Text = ngayTuyenDung.ToString("yyyy-MM-dd");
+                            staff.LblJob.Text = dataReader["TenCV"].ToString();
+                            staff.LblSalary.Text = dataReader["Luong"].ToString();
+
+                            flpanelListStaff.Controls.Add(staff);
+                        }
+                    }
+                }
+            }
+            foreach (Control control in flpanelListStaff.Controls)
+            {
+                if (control is UC_Staff staffControl)
+                {
+                    staffControl.OnDetailsUpdated += (s, e) => LoadStaff_Working();
+                }
+            }
+        }
+
+        public void LoadStaff_Layoff()
+        {
+            flpanelListStaff.Controls.Clear();
+            string sqlQuery = "SELECT * FROM V_ThongTinNhanVienNghiViec";
+            using (SqlConnection conn = new SqlConnection(conStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sqlQuery, conn))
+                {
+                    using (SqlDataReader dataReader = cmd.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            UC_Staff staff = new UC_Staff();
+                            staff.LblID.Text = dataReader["MaNV"].ToString();
+                            staff.LblName.Text = dataReader["HoTen"].ToString();
+                            staff.LblSex.Text = dataReader["GioiTinh"].ToString();
+                            DateTime dob = dataReader.GetDateTime(dataReader.GetOrdinal("NgaySinh"));
+                            staff.LblDob.Text = dob.ToString("yyyy-MM-dd");
+                            staff.LblAddress.Text = dataReader["DiaChi"].ToString();
+                            staff.LblPhone.Text = dataReader["SDT"].ToString();
+                            DateTime ngayTuyenDung = dataReader.GetDateTime(dataReader.GetOrdinal("NgayTuyenDung"));
+                            staff.LblNgayTuyenDung.Text = ngayTuyenDung.ToString("yyyy-MM-dd");
+                            staff.LblJob.Text = dataReader["TenCV"].ToString();
+                            staff.LblSalary.Text = dataReader["Luong"].ToString();
+
+                            flpanelListStaff.Controls.Add(staff);
+                        }
+                    }
+                }
+            }
+            foreach (Control control in flpanelListStaff.Controls)
+            {
+                if (control is UC_Staff staffControl)
+                {
+                    staffControl.OnDetailsUpdated += (s, e) => LoadStaff_Layoff();
+                }
+            }
+        }
+
         private void btnAdd_Click(object sender, EventArgs e)
         {
            FStaff_Add fStaff_Add = new FStaff_Add();
             if (fStaff_Add.ShowDialog() == DialogResult.OK)
             {
-                LoadStaff();
+                LoadStaff_Working();
             }
         }
 
@@ -113,5 +195,20 @@ namespace MilkTeaShop
             }
         }
 
+        private void cbbWorkStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbWorkStatus.SelectedIndex == 0)
+            {
+                LoadStaff();
+            }
+            else if (cbbWorkStatus.SelectedIndex == 1) 
+            {
+                LoadStaff_Working(); 
+            }
+            else
+            {
+                LoadStaff_Layoff();
+            }
+        }
     }
 }

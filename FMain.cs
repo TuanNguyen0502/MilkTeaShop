f345f416e3a664f8967b889c7ea380432e933901
Tuan <nguyenhahongtuan@gmail.com>
2024-04-24 23:06:18 +0700

    Rename form and add to Main Form

--- FMain.cs
+++ FMain.cs
@@ -88,5 +88,10 @@ namespace MilkTeaShop
         {
             OpenChildForm(new FImportIngredients());
         }
+
+        private void button_xuatNL_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FExport_Ingredients());
+        }
     }
 }

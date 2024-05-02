9de7249f524b5089ee86faad919da41bb8d7d92f
Minh <minhvuongakira@gmail.com>
2024-05-02 08:41:49 +0700

    done

--- FManagerAccount.Designer.cs
+++ FManagerAccount.Designer.cs
@@ -34,12 +34,11 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_Home = new Guna.UI2.WinForms.Guna2Button();
             this.btn_Exit = new Guna.UI2.WinForms.Guna2Button();
             this.btn_Delete = new Guna.UI2.WinForms.Guna2Button();
-            this.btn_Edit = new Guna.UI2.WinForms.Guna2Button();
             this.label2 = new System.Windows.Forms.Label();
             this.pnlCenter = new System.Windows.Forms.Panel();
-            this.btn_Home = new Guna.UI2.WinForms.Guna2Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -58,7 +57,7 @@
             this.btn_Add.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.btn_Add.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btn_Add.ForeColor = System.Drawing.Color.White;
-            this.btn_Add.Location = new System.Drawing.Point(18, 176);
+            this.btn_Add.Location = new System.Drawing.Point(18, 193);
             this.btn_Add.Name = "btn_Add";
             this.btn_Add.Size = new System.Drawing.Size(202, 65);
             this.btn_Add.TabIndex = 0;
@@ -91,7 +90,6 @@
             this.panel2.Controls.Add(this.btn_Home);
             this.panel2.Controls.Add(this.btn_Exit);
             this.panel2.Controls.Add(this.btn_Delete);
-            this.panel2.Controls.Add(this.btn_Edit);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.btn_Add);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
@@ -99,6 +97,22 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(247, 609);
             this.panel2.TabIndex = 2;
+            // 
+            // btn_Home
+            // 
+            this.btn_Home.BorderRadius = 8;
+            this.btn_Home.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Home.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Home.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_Home.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_Home.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btn_Home.ForeColor = System.Drawing.Color.White;
+            this.btn_Home.Location = new System.Drawing.Point(18, 92);
+            this.btn_Home.Name = "btn_Home";
+            this.btn_Home.Size = new System.Drawing.Size(202, 65);
+            this.btn_Home.TabIndex = 5;
+            this.btn_Home.Text = "Home";
+            this.btn_Home.Click += new System.EventHandler(this.btn_Home_Click);
             // 
             // btn_Exit
             // 
@@ -109,7 +123,7 @@
             this.btn_Exit.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.btn_Exit.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btn_Exit.ForeColor = System.Drawing.Color.White;
-            this.btn_Exit.Location = new System.Drawing.Point(18, 498);
+            this.btn_Exit.Location = new System.Drawing.Point(18, 397);
             this.btn_Exit.Name = "btn_Exit";
             this.btn_Exit.Size = new System.Drawing.Size(202, 65);
             this.btn_Exit.TabIndex = 4;
@@ -125,28 +139,12 @@
             this.btn_Delete.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.btn_Delete.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btn_Delete.ForeColor = System.Drawing.Color.White;
-            this.btn_Delete.Location = new System.Drawing.Point(18, 386);
+            this.btn_Delete.Location = new System.Drawing.Point(18, 294);
             this.btn_Delete.Name = "btn_Delete";
             this.btn_Delete.Size = new System.Drawing.Size(202, 65);
             this.btn_Delete.TabIndex = 3;
             this.btn_Delete.Text = "Xóa tài khoản";
             this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
-            // 
-            // btn_Edit
-            // 
-            this.btn_Edit.BorderRadius = 8;
-            this.btn_Edit.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.btn_Edit.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.btn_Edit.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.btn_Edit.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btn_Edit.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.btn_Edit.ForeColor = System.Drawing.Color.White;
-            this.btn_Edit.Location = new System.Drawing.Point(18, 281);
-            this.btn_Edit.Name = "btn_Edit";
-            this.btn_Edit.Size = new System.Drawing.Size(202, 65);
-            this.btn_Edit.TabIndex = 2;
-            this.btn_Edit.Text = "Sửa tài khoản";
-            this.btn_Edit.Click += new System.EventHandler(this.btn_Edit_Click);
             // 
             // label2
             // 
@@ -166,22 +164,6 @@
             this.pnlCenter.Name = "pnlCenter";
             this.pnlCenter.Size = new System.Drawing.Size(922, 609);
             this.pnlCenter.TabIndex = 3;
-            // 
-            // btn_Home
-            // 
-            this.btn_Home.BorderRadius = 8;
-            this.btn_Home.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.btn_Home.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.btn_Home.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.btn_Home.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btn_Home.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.btn_Home.ForeColor = System.Drawing.Color.White;
-            this.btn_Home.Location = new System.Drawing.Point(18, 84);
-            this.btn_Home.Name = "btn_Home";
-            this.btn_Home.Size = new System.Drawing.Size(202, 65);
-            this.btn_Home.TabIndex = 5;
-            this.btn_Home.Text = "Home";
-            this.btn_Home.Click += new System.EventHandler(this.btn_Home_Click);
             // 
             // FManagerAccount
             // 
@@ -212,7 +194,6 @@
         private System.Windows.Forms.Panel panel2;
         private Guna.UI2.WinForms.Guna2Button btn_Exit;
         private Guna.UI2.WinForms.Guna2Button btn_Delete;
-        private Guna.UI2.WinForms.Guna2Button btn_Edit;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel pnlCenter;
         private Guna.UI2.WinForms.Guna2Button btn_Home;
